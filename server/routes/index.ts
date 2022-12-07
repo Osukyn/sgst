1ae049271add2cc5cc327f93b2ddb3c6255f7a9c
Thybault <thybault.tonne@gmail.com>
2022-12-07 03:57:07 +0100

    Add routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,49 +1,58 @@
 import { Router } from "express";
 
-import {
-  findAll, findOne
-} from "../controllers/stand";
+const stand = require('../controllers/stand');
+const achatmunition = require('../controllers/achatmunition');
+const arme = require('../controllers/arme');
+const creneau = require('../controllers/creneau');
+const habilitation = require('../controllers/habilitation');
+const licencie = require('../controllers/licencie');
+const moniteur = require('../controllers/moniteur');
+const munition = require('../controllers/munition');
+const pas = require('../controllers/pas');
+const personne = require('../controllers/personne');
+const possedeHabilitation = require('../controllers/possedeHabilitation');
+const reservation = require('../controllers/reservation');
 
 
 
 const router = Router();
 
 
-router.get("/stand/:id", findOne);
-router.get("/stand", findAll);
+router.get("/stand/:id", stand.findOne);
+router.get("/stand", stand.findAll);
 
-router.get("/achatmunition/:id", findOne);
-router.get("/achatmunition", findAll);
+router.get("/achatmunition/:id", achatmunition.findOne);
+router.get("/achatmunition", achatmunition.findAll);
 
-router.get("/arme/:id", findOne);
-router.get("/arme", findAll);
+router.get("/arme/:id", arme.findOne);
+router.get("/arme", arme.findAll);
 
-router.get("/creneau/:id", findOne);
-router.get("/creneau", findAll);
+router.get("/creneau/:id", creneau.findOne);
+router.get("/creneau", creneau.findAll);
 
-router.get("/habilitation/:id", findOne);
-router.get("/habilitation", findAll);
+router.get("/habilitation/:id", habilitation.findOne);
+router.get("/habilitation", habilitation.findAll);
 
-router.get("/licencie/:id", findOne);
-router.get("/licencie", findAll);
+router.get("/licencie/:id", licencie.findOne);
+router.get("/licencie", licencie.findAll);
 
-router.get("/moniteur/:id", findOne);
-router.get("/moniteur", findAll);
+router.get("/moniteur/:id", moniteur.findOne);
+router.get("/moniteur", moniteur.findAll);
 
-router.get("/munition/:id", findOne);
-router.get("/munition", findAll);
+router.get("/munition/:id", munition.findOne);
+router.get("/munition", munition.findAll);
 
-router.get("/pas/:id", findOne);
-router.get("/pas", findAll);
+router.get("/pas/:id", pas.findOne);
+router.get("/pas", pas.findAll);
 
-router.get("/personne/:id", findOne);
-router.get("/personne", findAll);
+router.get("/personne/:id", personne.findOne);
+router.get("/personne", personne.findAll);
 
-router.get("/possedeHabilitation/:id", findOne);
-router.get("/possedeHabilitation", findAll);
+router.get("/possedeHabilitation/:id", possedeHabilitation.findOne);
+router.get("/possedeHabilitation", possedeHabilitation.findAll);
 
-router.get("/reservation/:id", findOne);
-router.get("/reservation", findAll);
+router.get("/reservation/:id", reservation.findOne);
+router.get("/reservation", reservation.findAll);
 
 export default router;
 
